4bccdd2fca8be10cd96a504cd1bdf28918f1fc61
Zackptg5 <zackptg5@comcast.net>
2017-12-13 18:33:57 -0500

    Update to aml 2.5

--- README.md
+++ README.md
@@ -14,6 +14,18 @@ This module enables ViPER4Android FX. [More details in support thread](https://f
 * Works with [AM3D Zirene Sound](https://forum.xda-developers.com/android/apps-games/mod-zirene-sound-am3d-t3396698/post71580634#post71580634), [Dolby Atmos](https://github.com/therealahrion/Dolby-Atmos-ZTE-Axon-7), & ViPER4Android XHiFi
 
 ## Change Log
+### v2.5 - 12.13.2017
+* Updated to Unity/AML v2.5
+* Added fix for some oreo devices like pixel 2
+* Added material v4a as an option - thanks to @pittvandewitt @xda-developers
+
+### v2.4 - 11.30.2017
+* Updated to Unity/AML v2.4
+
+### v2.3 - 11.28.2017
+* Updated to Unity/AML v2.3
+* Added old v4a (2.3.4 with super quality driver). Can choose between it and the newer one during install
+
 ### v2.2 - 11.08.2017
 * Updated to Unity/AML v2.2 - Please clean install Audio Modification Library and all modules (this will be the last version you'll have to clean install so in the future the auto clean install/upgrade script will take effect)
 * Added build prop compatibility fixes found in old V4A versions
--- common/aml-patches.sh
+++ common/aml-patches.sh
@@ -1,7 +1,13 @@
 ui_print "    Patching existing audio_effects files..."
-for CFG in $CONFIG_FILE $HTC_CONFIG_FILE $OTHER_V_FILE $OFFLOAD_CONFIG $V_CONFIG_FILE; do
-  if [ -f $CFG ]; then
-    sed -i 's/^effects {/effects {\n  v4a_standard_fx {\n    library v4a_fx\n    uuid 41d3c987-e6cf-11e3-a88a-11aba5d5c51b\n  }/g' $AMLPATH$CFG
-    sed -i 's/^libraries {/libraries {\n  v4a_fx {\n    path \/system\/lib\/soundfx\/libv4a_fx_ics.so\n  }/g' $AMLPATH$CFG
+for FILE in ${CFGS}; do
+  if [ ! "$(grep "v4a_fx" $AMLPATH$FILE)" ] && [ ! "$(grep "v4a_standard_fx" $AMLPATH$FILE)" ]; then
+    sed -i "s/^effects {/effects {\n  v4a_standard_fx {\n    library v4a_fx\n    uuid 41d3c987-e6cf-11e3-a88a-11aba5d5c51b\n  }/g" $AMLPATH$FILE
+    sed -i "s/^libraries {/libraries {\n  v4a_fx {\n    path $LIBPATCH\/lib\/soundfx\/libv4a_fx_ics.so\n  }/g" $AMLPATH$FILE
+  fi
+done
+for FILE in ${CFGSXML}; do
+  if [ ! "$(grep "v4a_fx" $AMLPATH$FILE)" ] && [ ! "$(grep "v4a_standard_fx" $AMLPATH$FILE)" ]; then
+    sed -i "/<libraries>/ a\        <library name=\"v4a_fx\" path=\"libv4a_fx_ics.so\"\/>" $AMLPATH$FILE
+    sed -i "/<effects>/ a\        <effect name=\"v4a_standard_fx\" library=\"v4a_fx\" uuid=\"41d3c987-e6cf-11e3-a88a-11aba5d5c51b\"\/>" $AMLPATH$FILE
   fi
 done
