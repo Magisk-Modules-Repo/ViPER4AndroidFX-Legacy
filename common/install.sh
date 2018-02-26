d8b9ca3d80f06916f7b593432e4502ff857010a9
Zackptg5 <zackptg5@comcast.net>
2018-02-25 23:43:38 -0500

    v1.5

--- common/install.sh
+++ common/install.sh
@@ -1,7 +1,7 @@
 rm -rf /data/app/com.pittvandewitt.viperfx /data/app/com.vipercn.viper4android* /data/app/com.audlabs.viperfx*
 
 # Device specific sepolicy patches
-if device_check "walleye" || device_check "taimen"; then
+if device_check "walleye" || device_check "taimen" || device_check "mata"; then
   test -f $SYS/lib/libstdc++.so && cp_ch $SYS/lib/libstdc++.so $UNITY$VEN/lib/libstdc++.so
 fi
 
@@ -13,18 +13,22 @@ case $(basename $ZIP) in
   *mat*|*Mat*|*MAT*) MAT=true;;
 esac
 
+# Keycheck binary by someone755 @Github, idea for code below by Zappo @xda-developers
+chmod 755 $INSTALLER/common/keycheck
+FUNCTION=chooseport
+                                                                                    
 chooseport() {
-  if [ "$1" == "on" ]; then
+  if [ $1 -eq 1 ]; then
     ui_print "   Choose which V4A you want installed:"
     ui_print "   Vol+ = new (2.5.0.5), Vol- = old (2.3.4.0)"
     ui_print "   Old V4A will install super quality driver"
-  elif [ "$1" == "mn" ]; then
+  elif [ $1 -eq 2 ]; then
     ui_print "   Choose which new V4A you want installed:"
     ui_print "   Vol+ = material, Vol- = original"
   fi
   #note from chainfire @xda-developers: getevent behaves weird when piped, and busybox grep likes that even less than toolbox/toybox grep
   while (true); do
-    getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > $INSTALLER/events
+    (/system/bin/getevent -lc 1 2>&1 | /system/bin/grep VOLUME | /system/bin/grep " DOWN" > $INSTALLER/events) || { FUNCTION=chooseportold; chooseportold 1; break; }
     if (`cat $INSTALLER/events 2>/dev/null | /system/bin/grep VOLUME >/dev/null`); then
       break
     fi
@@ -36,14 +40,44 @@ chooseport() {
   fi
 }
 
+chooseportold() {
+  if [ $1 -eq 1 ]; then
+    ui_print "   ! Legacy device detected!"
+    ui_print "   ! Restarting selection w/ old keycheck method"
+    ui_print " "
+    ui_print "   Enter selection again:"
+  elif [ $1 -eq 2 ]; then
+    ui_print "   Choose which new V4A you want installed:"
+    ui_print "   Vol+ = material, Vol- = original"
+  fi
+  $INSTALLER/common/keycheck
+  SEL=$?
+  shift
+  if [ $SEL -eq 42 ]; then
+    return 0
+  elif [ $SEL -eq 21 ]; then
+    return 1
+  else
+    ui_print "Vol key not detected! Defaulting to Vol Up! "
+    return 0
+  fi
+}
+
 mkdir -p $INSTALLER/system/lib/soundfx
 ui_print " "
 ui_print "- Select Version -"
 if ! $OLD && ! $NEW && ! $MAT; then
-  chooseport on || OLD=true
+  if ! $FUNCTION 1; then
+    OLD=true
+  elif $FUNCTION 2; then
+    MAT=true
+  else
+    NEW=true
+  fi
 else
   ui_print "   V4A version specified in zipname!"
 fi
+
 if $OLD; then
   ui_print "   Old V4A will be installed"
   cp -f $INSTALLER/custom/Old/ViPER4AndroidFX.apk $INSTALLER/system/app/ViPER4AndroidFX/ViPER4AndroidFX.apk
@@ -52,7 +86,6 @@ if $OLD; then
   $LATESTARTSERVICE && sed -i 's/<ACTIVITY>/com.vipercn.viper4android_v2/g' $INSTALLER/common/service.sh
   LIBPATCH="\/system"; LIBDIR=$SYS; DYNAMICOREO=false
 else
-  $NEW || $MAT || { chooseport mn && MAT=true; }
   cp -f $INSTALLER/custom/libv4a_fx_jb_$ABI.so $INSTALLER/system/lib/soundfx/libv4a_fx_ics.so
   if $MAT; then
     ui_print "   Material V4A will be installed"
